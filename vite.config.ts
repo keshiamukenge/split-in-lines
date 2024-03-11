b76be6e7cfab5c767c6a203415aba0ace6a8eccb
keshiamukenge <mukenge.keshia@gmail.com>
2024-03-11 01:02:46 +0100

    fix: use css animation

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,6 @@ export default defineConfig({
     },
 		outDir: "lib",
     rollupOptions: {
-      external: ['gsap'],
       plugins: [
         typescriptPaths({
           preserveExtensions: true,
